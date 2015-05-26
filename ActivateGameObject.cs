93413f1709935b72c6ab194d70c1d5b9348f7ce8
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-26 23:02:05 +0200

    Create ActivateGameObject/Editor classes

---
+++ ActivateGameObject.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ActivateGameObjectEx {
+
+    public sealed class ActivateGameObject : MonoBehaviour {
+
+        #region CONSTANTS
+
+        public const string Version = "v0.1.0";
+        public const string Extension = "ActivateGameObject";
+
+        #endregion
+
+        #region DELEGATES
+        #endregion
+
+        #region EVENTS
+        #endregion
+
+        #region FIELDS
+
+        /// <summary>
+        /// Allows identify component in the scene file when reading it with
+        /// text editor.
+        /// </summary>
+#pragma warning disable 0414
+        [SerializeField]
+        private string componentName = "ActivateGameObject";
+#pragma warning restore0414
+
+        #endregion
+
+        #region INSPECTOR FIELDS
+
+        [SerializeField]
+        private string description = "Description";
+
+        /// <summary>
+        /// List of game object to activate. Game objects will be activate
+        /// in list order.
+        /// </summary>
+        [SerializeField]
+        private List<GameObject> gameObjects; 
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// Optional text to describe purpose of this instance of the component.
+        /// </summary>
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public List<GameObject> GameObjects {
+            get { return gameObjects; }
+            set { gameObjects = value; }
+        }
+
+        #endregion
+
+        #region UNITY MESSAGES
+
+        private void Awake() { }
+
+        private void FixedUpdate() { }
+
+        private void LateUpdate() { }
+
+        private void OnEnable() { }
+
+        private void Reset() { }
+
+        private void Start() { }
+
+        private void Update() { }
+
+        private void OnValidate() { }
+
+        #endregion
+
+        #region EVENT INVOCATORS
+        #endregion
+
+        #region EVENT HANDLERS
+        #endregion
+
+        #region METHODS
+
+        public void Activate() {
+
+        }
+        #endregion
+
+    }
+
+}
---
+++ Editor/ActivateGameObjectEditor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ActivateGameObjectEx {
+
+    [CustomEditor(typeof(ActivateGameObject))]
+    [CanEditMultipleObjects]
+    public sealed class ActivateGameObjectEditor : Editor {
+        #region FIELDS
+
+        private ActivateGameObject Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+        private SerializedProperty gameObjects;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            DrawGameObjectsList();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawGameObjectsList() {
+            ReorderableListGUI.Title("Game Objects");
+            ReorderableListGUI.ListField(gameObjects);
+        }
+
+        private void OnEnable() {
+            Script = (ActivateGameObject)target;
+
+            description = serializedObject.FindProperty("description");
+            gameObjects = serializedObject.FindProperty("gameObjects");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    ActivateGameObject.Version,
+                    ActivateGameObject.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/ActivateGameObject")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(ActivateGameObject));
+            }
+        }
+
+        #endregion METHODS
+    }
+
+}
